8f7f0475387c713c872c3f7370d5a070177cb01e
JimmyCheong <c7834@uw.edu>
2022-07-20 19:55:16 -0700

    added cinemachine camera follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform _transform;
+        [SerializeField] private Transform _followTarget;
+        [SerializeField] private float _followSpeed;
+        
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FollowTarget()
+        {
+
+        }
+    }
+}
